bdb11a89a2c2dd785ffd69f2476ce1db9bcc6e6d
ayushkothari1 <pahadiayush61@gmail.com>
2026-09-06 23:33:23 +0530

    render list

--- workshop1/src/Components/Greetings.jsx
+++ workshop1/src/Components/Greetings.jsx
@@ -1,8 +1,22 @@
+const fruits = [
+  { id: 1, name: "Apple" },
+  { id: 2, name: "Mango" },
+  { id: 3, name: "Cherry" },
+];
+
 function Greetings(props) {
   return (
-    <h1>
-      {props.isLoggedIn ? `Welcome to ${props.name}!` : `Please sign in.`}
-    </h1>
+    <>
+      <h1>
+        {props.isLoggedIn ? `Welcome to ${props.name}!` : `Please sign in.`}
+      </h1>
+      {fruits.map((fruit) => (
+        <div key={fruit.id}>
+          <h1>I love </h1>
+          <li>{fruit.name}</li>
+        </div>
+      ))}
+    </>
   );
 }
 
