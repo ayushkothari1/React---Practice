9a3d92aeb6a601adeb49ec7236eec82fc30ddd81
ayushkothari1 <pahadiayush61@gmail.com>
2026-09-12 20:49:04 +0530

    state

--- workshop4/src/App.jsx
+++ workshop4/src/App.jsx
@@ -8,6 +8,8 @@ export const ColorPicker = () => {
     console.log("Button");
   }
   const [color, setColor] = useState("#ffffff");
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState("hey");
   return (
     <>
       <div id="color-picker-container" style={{ backgroundColor: color }}>
@@ -26,8 +28,22 @@ export const ColorPicker = () => {
       </div> */}
 
       {/*  use of stopPropagation to stop bubbling  */}
-      <div onClick={() => console.log("Parent")}>
+      <div onClick={(e) => console.log("Parent")}>
         <button onClick={handleButtonClick}>Click</button>
+      </div>
+
+      <div>
+        <input
+          type="text"
+          className="input"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <h2>{input}</h2>
+      </div>
+      <div>
+        <h1>{count}</h1>
+        <button onClick={() => setCount((prev) => prev + 1)}>Count</button>
       </div>
     </>
   );
