ba3c0be2d90d30c4d730b24ef60f09a75d9be90a
ayushkothari1 <pahadiayush61@gmail.com>
2026-09-12 18:08:37 +0530

    bubbling

--- workshop4/src/App.jsx
+++ workshop4/src/App.jsx
@@ -3,16 +3,33 @@ import { useState } from "react";
 import "./App.css";
 
 export const ColorPicker = () => {
+  function handleButtonClick(event) {
+    event.stopPropagation();
+    console.log("Button");
+  }
   const [color, setColor] = useState("#ffffff");
   return (
-    <div id="color-picker-container" style={{ backgroundColor: color }}>
-      <input
-        id="color-input"
-        value={color}
-        type="color"
-        onChange={(e) => setColor(e.target.value)}
-      ></input>
-    </div>
+    <>
+      <div id="color-picker-container" style={{ backgroundColor: color }}>
+        <input
+          id="color-input"
+          value={color}
+          type="color"
+          onChange={(e) => setColor(e.target.value)}
+        ></input>
+      </div>
+      {/* React Event bubbling */}
+      {/* <div onClick={() => console.log("Parent")}>
+        <button onClick={(handleButtonClick) => console.log("button")}>
+          Click
+        </button>
+      </div> */}
+
+      {/*  use of stopPropagation to stop bubbling  */}
+      <div onClick={() => console.log("Parent")}>
+        <button onClick={handleButtonClick}>Click</button>
+      </div>
+    </>
   );
 };
 
